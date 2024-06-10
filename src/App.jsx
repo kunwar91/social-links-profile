aa8928190cab028a0ff47ab8c612424613bbfc23
kunwar <kunwar_91@hotmail.com>
2024-06-10 19:21:10 +0530

    initial layout implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
 import "./App.css";
 
 function App() {
+  const links = [
+    "GitHub",
+    "Frontend Mentor",
+    "LinkedIn",
+    "Twitter",
+    "Instagram",
+  ];
   return (
-    <main>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <main className="bg-[#141414] flex justify-center items-center h-screen w-screen text-center">
+      <div className="bg-[#1F1F1F] p-[40px] rounded-[12px]">
+        <img />
+        <p className="text-white">Jessica Randall</p>
+        <p className="text-[#C4F82A]">London, United Kingdom</p>
+        <div className="flex flex-col pt-[8px]">
+          {links.map((link, index) => {
+            return (
+              <a
+                className="text-white hover:text-[#333333] hover:bg-[#C4F82A] bg-[#333333] p-3 my-[8px] rounded-[8px] w-full text-center"
+                key={index}
+              >
+                {link}
+              </a>
+            );
+          })}
+        </div>
+      </div>
+      {/* <h1 className="text-3xl font-bold underline">Hello world!</h1> */}
     </main>
     // <>
     //   <div>

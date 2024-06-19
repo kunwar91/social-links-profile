cce9af722ed2263634a6c2cfe91a324c0e446b4b
kunwar <kunwar_91@hotmail.com>
2024-06-19 16:12:02 +0530

    add fonts, customize theme, mobile ui changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,24 +9,29 @@ function App() {
     "Twitter",
     "Instagram",
   ];
+  const user = {
+    name: "Jessica Randall",
+    location: "London, United Kingdom",
+    bio: '"Front-end developer and avid reader."',
+  };
   return (
-    <main className="bg-[#141414] flex justify-center items-center h-screen w-screen text-center">
-      <div className="bg-[#1F1F1F] p-[40px] rounded-[12px] flex justify-center items-center flex-col">
+    <main className="bg-off-black flex justify-center items-center h-screen w-screen text-center">
+      <div className="bg-dark-grey lg:p-10 p-6 rounded-xl flex justify-center items-center flex-col lg:w-96 w-80">
         <img
           src="./avatar-jessica.jpeg"
-          className="rounded-full w-[88px] h-[88px]"
+          className="rounded-full w-[88px] h-[88px] mb-6"
           alt="img"
         />
-        <p className="text-white">Jessica Randall</p>
-        <p className="text-[#C4F82A]">London, United Kingdom</p>
-        <p className="text-white my-6">
-          {`"Front-end developer and avid reader."`}
+        <p className="text-white font-[Inter-SemiBold] text-2xl">{user.name}</p>
+        <p className="text-neon-green font-inter-bold text-sm mt-1">
+          {user.location}
         </p>
+        <p className="text-white my-6 font-interregular text-sm">{user.bio}</p>
         <div className="flex flex-col w-full">
           {links.map((link, index) => {
             return (
               <a
-                className="text-white hover:text-[#333333] hover:bg-[#C4F82A] bg-[#333333] p-3 my-2 rounded-lg w-full text-center"
+                className="text-white hover:text-off-black hover:bg-neon-green bg-grey p-3 my-2 rounded-lg w-full text-center font-inter-bold text-sm"
                 key={index}
               >
                 {link}
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,27 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'inter-bold': ['Inter-Bold'],
+        'interregular': ['Inter-Regular'],
+      },
+      fontSize: {
+        sm: ['14px', '21px'],
+        base: ['16px', '24px'],
+        lg: ['20px', '28px'],
+        '2xl': ['24px', '36px'],
+      },
+      colors: {
+        'grey': '#333333',
+        'dark-grey': '#1F1F1F',
+        'off-black': '#141414',
+        'neon-green': '#C4F82A',
+      },
+      screens: {
+        'sm': '375px',
+      }
+    },
   },
   plugins: [],
 }
